bf536688ccca9fa7a368b11bfbe418b551539429
xxmoeedxx <moeed.ahmad@cloudmizan.com>
2024-10-12 23:01:23 +0500

    Add userProfile image

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -4,7 +4,7 @@ import '../assets/css/UserProfile.css';
 const UserProfile = () => {
   return (
     <div className="card" style={{ width: '18rem', margin: '20px auto', padding: '15px' }}>
-      <img src="/img/avatars/avatar.jpg" className="card-img-top rounded-circle" alt="User Avatar" style={{ width: '50%', margin: '0 auto' }} />
+      <img src="img/avatar.png" className="card-img-top rounded-circle" alt="User Avatar" style={{ width: '50%', margin: '0 auto' }} />
       <div className="card-body text-center">
         <h5 className="card-title">Charles Hall</h5>
         <p className="card-text text-muted">charles.hall@example.com</p>
